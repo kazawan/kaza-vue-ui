e5f21d17b22706ea980f2b24d0352f05d4dc6130
kazawan <55086403@qq.com>
2023-02-16 17:08:35 +0800

    Changes to be committed: 	modified:   kazaProgressBar.vue

--- kazaProgressBar.vue
+++ kazaProgressBar.vue
@@ -1,8 +1,10 @@
 <template>
-    
     <div class="bar">
         <div class="text">
             {{ getCount }}
+        </div>
+        <div class="title">
+            {{title}}
         </div>
         <svg class="box1 base" viewBox="0 0 210 210" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle class="c1 ani cc1" id="c1" cx="105" cy="105" r="90" :stroke="getColor" stroke-opacity="0.69"
@@ -15,8 +17,15 @@
     </div>
 </template>
   
-  
-<style lang="less">
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap');
+
+
+
+</style> 
+<style lang="less" scoped>
+
+
 @bgcolor: #7e807e00;
 @mainwidth: v-bind(width);
 
@@ -24,6 +33,7 @@
 @COLOR: #1e4adb;
 
 .base {
+    
     width: @mainwidth;
     height: @mainheight;
     // background-color: @bgcolor;
@@ -44,8 +54,21 @@
     height: @mainheight;
     
 }
+.title{
+    font-family: 'Oswald', sans-serif;
+    position: absolute;
+    top:v-bind(getTitleTop);
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    overflow: hidden;
+    font-size: v-bind(getTitleFont_size);
+    font-weight: 500;
+}
 
 .text {
+    font-family: 'Oswald', sans-serif;
     position: absolute;
     top: 50%;
     left: 50%;
@@ -111,7 +134,23 @@ const props = defineProps({
         type: String,
         default: '#ccc',
         required: false
+    },
+    title :{
+        type: String,
+        default: '',
+        required: false
+    },
+    titleTop:{
+        type: String,
+        default: '35%',
+        required: false
+    },
+    titleFont_size:{
+        type: String,
+        default: '10px',
+        required: false
     }
+    
 
 })
 
@@ -141,6 +180,18 @@ const getBoardColor = computed(()=>{
     return props.boardColor
     
 })
+
+const getTitleTop = computed(()=>{
+    return props.titleTop
+    
+})
+
+const getTitleFont_size = computed(()=>{
+    return props.titleFont_size
+    
+})
+
+
 
 
 
