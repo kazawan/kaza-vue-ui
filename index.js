9345ac7ebeb96c4ac150339d336f475085c97471
kazawan <55086403@qq.com>
2023-03-27 13:44:11 +0800

    添加组件:   index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import kazaProgressBar from './ProgressBar/kazaProgressBar.vue'
 import kCol from './kaza-layout/kCol.vue'
 import kRow from './kaza-layout/kRow.vue'
 import kSuperbody from './kaza-layout/kSuperbody.vue'
-
-
+import kCals from './cal/cals.vue'
+import kTodoCard from './cal/kazaTodoCard.vue'
 export default {
     install:(app)=>{
         app.component('kazaCharts', kazaCharts)
@@ -12,5 +12,7 @@ export default {
         app.component('k-col',kCol)
         app.component('k-row',kRow)
         app.component('k-superbody',kSuperbody)
+        app.component('kaza-cals',kCals)
+        app.component('kaza-todocard',kTodoCard)
     }
 }
